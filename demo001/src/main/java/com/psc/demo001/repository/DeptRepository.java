984f794e8fdb5dd86a89f8cd0f3783e009e2057d
simon <parkseungchul@gmail.com>
2020-09-04 18:37:54 +0900

    기본 페이지 추가

--- demo001/src/main/java/com/psc/demo001/repository/DeptRepository.java
+++ demo001/src/main/java/com/psc/demo001/repository/DeptRepository.java
@@ -1,8 +1,17 @@
 package com.psc.demo001.repository;
 
-import com.psc.demo001.domain.Dept;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import com.psc.demo001.domain.Dept;
+
 public interface DeptRepository extends CrudRepository<Dept, Integer>{
+	
+	List<Dept> findByDnameContaining(String dname);
+	
+	List<Dept> findByDnameContaining(String dname, Pageable pageable);
+	
 
 }
--- demo001/src/test/java/com/psc/demo001/repository/DeptTest.java
+++ demo001/src/test/java/com/psc/demo001/repository/DeptTest.java
@@ -10,6 +10,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -58,7 +61,7 @@ public class DeptTest {
 	}
 	
 	@Test
-	public void A005_부서_정보_단건_삭제(){
+	public void A003_부서_정보_단건_삭제(){
 		Dept dept = new Dept();
 		dept.setDeptno(10);
 		deptRepository.delete(dept);
@@ -66,17 +69,39 @@ public class DeptTest {
 	}
 	
 	@Test
-	public void A006_부서_정보_다건_삽입() {
+	public void A004_부서_정보_다건_삽입() {
 		List<Dept> deptList = new ArrayList<Dept>(); 
 		for(int i = 1000; i <2001; i++) {
 			Dept dept = new Dept();
 			dept.setDeptno(i);
-			dept.setDname(String.valueOf(dept));
-			dept.setLoc(String.valueOf(dept));
+			dept.setDname(String.valueOf(i));
+			dept.setLoc(String.valueOf(i));
 			deptList.add(dept);
 		}
 		deptRepository.saveAll(deptList);
 	}
+	
+	@Test
+	public void A005_부서명칭_검색() {
+		deptRepository.findByDnameContaining("00").forEach(dept -> {
+			log.debug(dept.toString());
+		});
+	}
+	
+	@Test
+	public void A006_부서명칭_검색_단순_페이징() {
+		Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "dname");
+		deptRepository.findByDnameContaining("00", pageable).forEach(dept -> {
+			log.debug(dept.toString());
+		});
+	}
+	
+	
+	@Test
+	public void A007_부서_정보_다건_삭제() {
+		deptRepository.deleteAll();
+	}
+	
 	
 	
 }
