5a91c09edc1c15e58442a8c4b7381c16fad670a9
parkseungchul <parkseungchul@gmail.com>
2020-09-08 22:28:28 +0900

    테스트

--- demo002/src/main/java/com/psc/demo001/service/DeptServiceImpl.java
+++ demo002/src/main/java/com/psc/demo001/service/DeptServiceImpl.java
@@ -13,8 +13,8 @@ public class DeptServiceImpl implements DeptService{
 	@Override
 	public List<DeptDto> listDept() {
 		List<DeptDto> deptDtoList = new ArrayList<DeptDto>();
-		for(int i = 10; i< 20; i++) {
-			DeptDto deptDto = new DeptDto(i, String.valueOf(i), String.valueOf(i));
+		for(int i = 10; i< 1000; i++) {
+			DeptDto deptDto = new DeptDto(i, "박신후 " + String.valueOf(i), String.valueOf(i));
 			deptDtoList.add(deptDto);
 		}
 		return deptDtoList;
