1f9d95d7bd93d2e648e67c7c4799a741b3a9f4d7
parkseungchul <parkseungchul@gmail.com>
2020-10-11 16:31:57 +0900

    demo00021 add

---
+++ demo0021/src/main/java/com/psc/demo0021/Demo0021Application.java
@@ -0,0 +1,13 @@
+package com.psc.demo0021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0021Application.class, args);
+	}
+
+}
---
+++ demo0021/src/test/java/com/psc/demo0021/Demo0021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.demo0021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
